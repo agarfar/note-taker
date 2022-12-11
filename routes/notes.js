54f225729d7872dd7f4e7ff792fd9c358d524070
Soprano <antfar67@gmail.com>
2022-12-10 18:25:48 -0800

    Successfully use delete route to update db.json, but page does not automatically update the note list on the page. Need to fix

--- routes/notes.js
+++ routes/notes.js
@@ -1,13 +1,14 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
+const fs = require('fs');
 
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// POST Route for a new UX/UI tip
+// POST Route for a new note
 notes.post('/', (req, res) => {
     console.log(req.body);
 
@@ -26,5 +27,31 @@ notes.post('/', (req, res) => {
         res.error('Error in adding note');
     }
 });
+
+// DELETE route for removing note from web page
+notes.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    // console.log(req.body);
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            // Convert string into JSON object
+            const parsedNotes = JSON.parse(data);
+            const deleteNote = parsedNotes.filter(note => note.id !== id);
+            // Write updated reviews back to the file
+            fs.writeFile(
+                './db/db.json',
+                JSON.stringify(deleteNote),
+                (writeErr) =>
+                    writeErr
+                        ? console.error(writeErr)
+                        : console.info('Successfully deleted note!')
+            );
+        }
+    });
+})
+
 
 module.exports = notes;
