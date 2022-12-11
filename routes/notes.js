510e6bdc51f4460121b685cfbd0d3021407aacb2
Soprano <antfar67@gmail.com>
2022-12-10 17:41:39 -0800

    Created wildcard route for homepage, and successfully tested backend functionality. Need to create delete function and push to heroku.

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,7 @@ const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
-// GET Route for retrieving all the tips
+// GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const PORT = 3001;
 
 const app = express();
 
-// Import custom middleware, "cLog"
-// app.use(clog);
-
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,13 +19,13 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
 // GET Route for note page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
